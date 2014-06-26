20934f5cb80958170efcf50485bb96434f2a9e8a
Amit Kulkarni <yuvraj2987@gmail.com>
2014-06-26 11:06:09 -0700

    Added driver.py to run the web crawler

---
+++ src/driver.py
@@ -0,0 +1,25 @@
+from Crawler import PageParser
+from Queue import Queue
+
+num_threads = 1
+max_size = 10
+start_url = "http://slickdeals.net/forums/forumdisplay.php?f=9"
+domain= "http://slickdeals.net"
+page_queue = Queue(maxsize = max_size)
+post_queue = Queue(maxsize = max_size)
+url = None
+
+page_parser_threadList = [PageParser.Thread(page_queue, post_queue) for i in xrange(num_threads)]
+
+page_queue.put(start_url, True)
+#Start Threads
+for thread in page_parser_threadList:
+		thread.start()
+
+for page_num in range(2, 3):
+		url = start_url+"&page="+str(page_num)
+		page_queue.put(url, True)
+
+
+
+
