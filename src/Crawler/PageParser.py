fa04f942dbcd1c0067f9026d2b0c34b1cc04f57b
Amit Kulkarni <yuvraj2987@gmail.com>
2014-06-27 08:16:20 -0700

    print statements for debugging.. commented out

--- src/Crawler/PageParser.py
+++ src/Crawler/PageParser.py
@@ -16,7 +16,7 @@ class Thread(threading.Thread):
         while True:
             try:
                 url = self.in_queue.get(True)
-                #print url
+                print url
                 #process url
                 html_response = requests.get(url)
                 if html_response is None:
@@ -27,14 +27,18 @@ class Thread(threading.Thread):
                 aref_list = soup.find_all('a')
                 if aref_list is None:
                     continue
-
+                #print "Found refs"
                 for aref in aref_list:
                     if aref.has_attr('id') and re.match(r'thread_title_*', aref.get('id')):
                         if aref.has_attr('href'):
                             out_url = self.domain+str(aref.get('href'))
+                            #print "out url:", out_url
                             self.out_queue.put(out_url, True)
+                            #print "url added to the queue"
                         #href if ends
                     #aref if ends
+                #aref_list for ends
+                #print "Get next page"
 
             except requests.exceptions.RequestException:
                 pass
